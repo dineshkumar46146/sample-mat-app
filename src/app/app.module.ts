08b926ab8337eee15f600c564a16329f9294e9be
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-10-02 23:41:50 +0530

    one page material app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, 
+  MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, 
+  MatSortModule, MatNativeDateModule } from '@angular/material';
+import { MyTableComponent } from './my-table/my-table.component';
+import { BodyComponent } from './body/body.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 
+
+
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    MyTableComponent,
+    BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+   MatCheckboxModule,
+   MatDatepickerModule,
+   MatSelectModule,
+   MatSliderModule ,
+   MatNativeDateModule,
+   MatInputModule,
+   MatRadioModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
